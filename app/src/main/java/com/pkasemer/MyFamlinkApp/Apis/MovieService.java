de6dfc4c7825ad4e86f5f533b50cb9d20a1e5076
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-03-30 21:43:13 +0300

    added base for appointments

--- app/src/main/java/com/pkasemer/MyFamlinkApp/Apis/MovieService.java
+++ app/src/main/java/com/pkasemer/MyFamlinkApp/Apis/MovieService.java
@@ -1,6 +1,7 @@
 package com.pkasemer.MyFamlinkApp.Apis;
 
 
+import com.pkasemer.MyFamlinkApp.Models.Appointment;
 import com.pkasemer.MyFamlinkApp.Models.PostResponse;
 import com.pkasemer.MyFamlinkApp.Models.Referal;
 
@@ -11,11 +12,21 @@ import retrofit2.http.POST;
 
 public interface MovieService {
 
-//    http://192.168.88.190:8080/projects/myfamLinkApp/mobile/api/v1/refercase/create_case.php
     //post refer case
-    @POST("refercase/create_case.php")
+
+    //    http://192.168.88.190:8080/projects/myfamLinkApp/mobile/api/v1/refercase/create_case.php
+    @POST("routes/create_case.php")
     @Headers("Cache-Control: no-cache")
     Call<PostResponse> postReferCase(
-            @Body Referal createAddress
+            @Body Referal createCase
+    );
+
+    //post appointment
+
+//    http://192.168.88.190:8080/projects/myfamLinkApp/mobile/api/v1/routes/create_appointment.php
+    @POST("routes/create_appointment.php")
+    @Headers("Cache-Control: no-cache")
+    Call<PostResponse> postAppointment(
+            @Body Appointment createAppointment
     );
 }
---
+++ app/src/main/java/com/pkasemer/MyFamlinkApp/Models/Appointment.java
@@ -0,0 +1,78 @@
+package com.pkasemer.MyFamlinkApp.Models;
+
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Appointment {
+
+    @SerializedName("userid")
+    @Expose
+    private String userid;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("purpose")
+    @Expose
+    private String purpose;
+    @SerializedName("appointment_date")
+    @Expose
+    private String appointmentDate;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+}
