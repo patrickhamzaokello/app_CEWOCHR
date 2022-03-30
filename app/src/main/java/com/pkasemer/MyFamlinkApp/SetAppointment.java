92389800ebaa89046ff9da6f9d6313bd91e9b7cd
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-03-30 18:23:21 +0300

    setting appointment ui

--- app/src/main/java/com/pkasemer/MyFamlinkApp/SetAppointment.java
+++ app/src/main/java/com/pkasemer/MyFamlinkApp/SetAppointment.java
@@ -3,10 +3,23 @@ package com.pkasemer.MyFamlinkApp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class SetAppointment extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class SetAppointment extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
     ActionBar actionBar;
+    TextView tvDate;
+    MaterialButton btPickDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,21 @@ public class SetAppointment extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+
+
+        tvDate = findViewById(R.id.tvDate);
+        btPickDate = findViewById(R.id.btPickDate);
+
+        btPickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Please note that use your package name here
+                com.pkasemer.MyFamlinkApp.DatePicker mDatePickerDialogFragment;
+                mDatePickerDialogFragment = new com.pkasemer.MyFamlinkApp.DatePicker();
+                mDatePickerDialogFragment.show(getSupportFragmentManager(), "DATE PICK");
+            }
+        });
     }
 
 
@@ -26,5 +54,16 @@ public class SetAppointment extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+        tvDate.setText(selectedDate);
+        tvDate.setVisibility(View.VISIBLE);
     }
 }
